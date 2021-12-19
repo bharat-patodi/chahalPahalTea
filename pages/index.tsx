cf203c908e13472e164139725d4d81166de774b8
Bharat Patodi <bharatpatodi@gmail.com>
2021-12-19 23:42:44 +0530

    Fix home page flex issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,72 +15,66 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div className="container">
-        <main>
-          <section className={styles.allProducts}>
-            <h1>चहल पहल चाय</h1>
-            <div className={styles.mainProduct}>
-              <picture>
-                <img src="product-large.png" alt="product-1" />
-              </picture>
-              <div className={styles.productDetails}>
-                <p>
-                  चहल पहल चाय पसंद करने के लिए आपका धन्यवाद। चाय केवल एक पेय
-                  नहीं है। चाय हर सुबह की ताज़गी का एक ज़रिया है। और हमारे दिन का
-                  एक अभिन्न हिस्सा है।
-                </p>
-                <br />
-                <p>
-                  चहल पहल चाय में हैं चुनी हुई पत्तियाँ, जो अपने में संजोये हुए
-                  हैं आपकी पसंदीदा ख़ुशबू और ज़ायका। चहल पहल चाय आप तक लाता है
-                  जैन परिवार, इसीलिए आप हमारी सच्चाई और चाय की गुणवत्ता पर पूरा
-                  विश्वास कर सकते हैं।
-                </p>
-                <br />
-                <p>अपने आप को कुछ क्षणों का आराम दें, और चाय का आनंद लें।</p>
+      <main className="container">
+        <section className={styles.allProducts}>
+          <h1>चहल पहल चाय</h1>
+          <div className={styles.mainProduct}>
+            <picture>
+              <img src="product-large.png" alt="product-1" />
+            </picture>
+            <div className={styles.productDetails}>
+              <p>
+                चहल पहल चाय पसंद करने के लिए आपका धन्यवाद। चाय केवल एक पेय नहीं
+                है। चाय हर सुबह की ताज़गी का एक ज़रिया है।
+                  और हमारे दिन का एक
+                अभिन्न हिस्सा है। चहल पहल चाय में हैं चुनी हुई पत्तियाँ, जो अपने
+                में संजोये हुए हैं आपकी पसंदीदा ख़ुशबू और ज़ायका। चहल पहल चाय आप
+                तक लाता है जैन परिवार, इसीलिए आप हमारी सच्चाई और चाय की गुणवत्ता
+                पर पूरा विश्वास कर सकते हैं। अपने आप को कुछ क्षणों का आराम दें,
+                और चाय का आनंद लें।
+              </p>
                 {/* <p>
-                  M.R.P.: <span className={styles.strikeThrough}>₹250</span>
-                </p>
-                <p>Price: ₹250</p>
-                <p>You Save: ₹250 (45%) Inclusive of all taxes</p>
-                <ul className={styles.productDetails}>
-                  About the product
-                  <li>Detail 1</li>
-                  <li>Detail 2</li>
-                  <li>Detail 3</li>
-                  <li>Detail 4</li>
-                </ul>
-                <form action="">
-                  <label htmlFor="weight">Weight:</label>
-                  <select name="weight" id="weight">
-                    <option value="100gm">100gm</option>
-                    <option value="250g">250gm</option>
-                    <option value="500gm">500gm</option>
-                    <option value="1kg">1kg</option>
-                  </select>
-                  <label htmlFor="quantity">Quantity:</label>
-                  <select name="quantity" id="quantity">
-                    <option value="100gm">1</option>
-                    <option value="250g">2</option>
-                    <option value="500gm">3</option>
-                    <option value="1kg">4</option>
-                  </select>
-                  <input type="submit" value="खरीदें"></input>
-                </form> */}
-              </div>
+                    M.R.P.: <span className={styles.strikeThrough}>₹250</span>
+                  </p>
+                  <p>Price: ₹250</p>
+                  <p>You Save: ₹250 (45%) Inclusive of all taxes</p>
+                  <ul className={styles.productDetails}>
+                    About the product
+                    <li>Detail 1</li>
+                    <li>Detail 2</li>
+                    <li>Detail 3</li>
+                    <li>Detail 4</li>
+                  </ul>
+                  <form action="">
+                    <label htmlFor="weight">Weight:</label>
+                    <select name="weight" id="weight">
+                      <option value="100gm">100gm</option>
+                      <option value="250g">250gm</option>
+                      <option value="500gm">500gm</option>
+                      <option value="1kg">1kg</option>
+                    </select>
+                    <label htmlFor="quantity">Quantity:</label>
+                    <select name="quantity" id="quantity">
+                      <option value="100gm">1</option>
+                      <option value="250g">2</option>
+                      <option value="500gm">3</option>
+                      <option value="1kg">4</option>
+                    </select>
+                    <input type="submit" value="खरीदें"></input>
+                  </form> */}
             </div>
-          </section>
-          {/* <section className={styles.popularActions}>
+          </div>
+        </section>
+        {/* <section className={styles.popularActions}>
             <Card name="Tea Subscription" />
             <Card name="Top Selling Products" />
             <Card name="Sign In" />
             <Card name="Shop By Category" />
           </section> */}
-          {/* <section className={styles.dailyDeals}>
+        {/* <section className={styles.dailyDeals}>
             <h2>Testimonials</h2>
           </section> */}
-        </main>
-      </div>
+      </main>
       <Footer />
     </div>
   );
