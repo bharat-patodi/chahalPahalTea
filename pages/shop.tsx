3f95c839beb76a30232a82b1caa76258a7bb2778
Bharat Patodi <bharatpatodi@gmail.com>
2022-11-14 11:37:22 +0530

    Add shop placeholder image

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import styles from "../styles/Shop.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 const Shop = () => {
   return (
@@ -27,7 +28,23 @@ const ProductView = (props: any) => {
     <>
       <div className={styles.productView}>
         <h3>{props.name}</h3>
+        <img
+          src="https://images.unsplash.com/photo-1544787219-7f47ccb76574?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1921&q=80"
+          width="20%"
+        />
         <p>Product Details</p>
+        <label htmlFor="">
+          Weight
+          <select name="quantity" id="">
+            <option value="250gm">250gm</option>
+            <option value="500gm">500gm</option>
+            <option value="1kg">1kg</option>
+          </select>
+        </label>
+        <label>
+          Quantity
+          <input type="text" />
+        </label>
         <Link href="/cart">
           <a>
             <button>Add to Cart</button>
