36850de0164da8ee675c4389ad56d7bc7eea2ca0
Bharat Patodi <bharatpatodi@gmail.com>
2022-11-14 11:36:31 +0530

    Add returns page content

---
+++ pages/returns.tsx
@@ -0,0 +1,42 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Returns = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <div>
+          <h1>Returns</h1>
+          <p>
+            Cancellation of order All orders placed through chahalpahaltea.com
+            are processed with a very short turnaround time and hence
+            cancellation of orders are not possible. Return policy Returns will
+            be accepted only for quality concerns or damage of items in transit.
+            Concerns should be raised to tejpan.biz@gmail.com within 48 hours of
+            receipt with details of the order. In case of qualitative
+            inferiority of product, total value of the product as well as
+            courier charges borne by customer will be refunded back to customer
+            account. Products acknowledged as ‘damaged’ or ‘defective’ will be
+            replaced by us on receipt of the damaged product. Once we receive
+            the item at our facility we will verify its condition and initiate
+            the process of refund/replacement accordingly. REFUND POLICY Refund
+            for Orders once placed shall not be possible under normal
+            circumstances once shipping is carried out. Please contact us by
+            email to tejpan.biz@gmail.com or in the customer care number
+            available in the home page for any queries. Shop our products online
+            at www.chahalpahaltea.com from anywhere in India and avail fabulous
+            offers and discounts. Follow us on our Social Media pages: for
+            regular updates on Chahal Pahal Tea.
+            https://www.instagram.com/chahal_pahal_tea/
+            https://www.facebook.com/chahalpahaltea/ Visit our Blogs
+            https://www.chahalpahaltea.com/blog/ for useful and interesting
+            information about Tea.
+          </p>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+export default Returns;
