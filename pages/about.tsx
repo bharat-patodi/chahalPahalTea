bc8049703d7056ba85789c734e841e758c60286a
Bharat Patodi <bharatpatodi@gmail.com>
2022-11-14 13:02:25 +0530

    Add about us page content

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,7 +8,16 @@ const About = () => {
       <main>
         <div>
           <h1>About</h1>
-          <p>This is the about page</p>
+          <p>
+            Chahal Pahal Tea brand owned by Tejpan Enterprises. This
+            organization come with rich 50 years of experience of spice and tea.
+            Comes from Jain family where providing quality products is pivotal
+            priority. Chahal Pahal Tea is new age brand which is customer
+            centric and is continuously growing on customer demand. Chahal Pahal
+            Premium tea is a quality blend of tea which provided right taste and
+            color. Elaichi and adrak flavor comes with original spices and “zero
+            artificial flavors”.
+          </p>
         </div>
       </main>
       <Footer />
