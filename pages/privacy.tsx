f5576eb8761c3642699abe56f69b2b4603b69cfd
Bharat Patodi <bharatpatodi@gmail.com>
2022-11-14 11:36:20 +0530

    Add privacy page content

---
+++ pages/privacy.tsx
@@ -0,0 +1,127 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Privacy = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <div>
+          <h1>Privacy</h1>
+          <p>
+            chahalpahaltea.com is committed to protecting the privacy of
+            visitors to this site (the "Site"). At chahalpahaltea.com, we want
+            you to have an enjoyable shopping experience. And while it is
+            necessary for us to collect certain personal information, we respect
+            and protect your right to privacy as set forth in this Privacy
+            Policy. This Privacy Policy applies to the Site. This Privacy Policy
+            does not apply to other web sites to which we link. Note: Our
+            privacy policy is subject to change at any time without notice. To
+            make sure you are aware of any changes, please review this policy
+            periodically. By visiting this Website you agree to be bound by the
+            terms and conditions of this Privacy Policy. If you do not agree
+            please do not use or access our Website. By mere use of the Website,
+            you expressly consent to our use and disclosure of your personal
+            information in accordance with this Privacy Policy. This Privacy
+            Policy is incorporated into and subject to the Terms of Use. Cookie
+            Policy In order to access this Site, you must first complete the
+            registration process. During the registration process, we collect
+            personal information such as your name, Contact Number and email
+            address. Once you complete and submit your registration, you have
+            opted in to receive email communication from us. We also collect
+            personal information when you choose to use certain other features
+            of the Site, such as making purchases or electing to receive text
+            messages about upcoming promotions or events. When you choose to use
+            these additional features, we require you to provide additional
+            personal information such as your phone number, billing and shipping
+            addresses and credit card information, and we may request additional
+            personal information such as your shopping preferences and
+            demographics. In addition, through the Site, we automatically gather
+            certain information about the use of the Site, such as how
+            frequently certain areas of the Site are visited, including through
+            the use of cookies, web beacons and other technologies. Most
+            browsers can be set to prevent cookies or to notify you when one is
+            being placed. However, cookies are necessary to access the sit
+            including through the use of cookies, web beacons and other
+            technologies. Most browsers can be set to prevent cookies or to
+            notify you when one is being placed. However, cookies are necessary
+            to access the Site. How We Use Your Information We will not share or
+            disclose your information to anyone except as described in this
+            Privacy Policy. We use personally identifiable information in order
+            to give you a more enjoyable, convenient shopping experience and to
+            help us identify and/or provide information, products or services
+            that may be of interest to you. We use your personally identifiable
+            information to support and enhance your use of the Site and its
+            features, including without limitation: fulfilling your order;
+            providing customer service; tracking email invitations you send; and
+            otherwise supporting your use of the Site and its features. We may
+            also track your past purchases to provide you with a personalized
+            profile of your shopping history. In addition, we may notify you
+            about new services or special promotional programs, or send you
+            offers or information. We may permit certain trusted third parties
+            to access your information in connection with their performance of
+            services to maintain and operate the Site and certain features on
+            the Site. For example, we may use third parties to host the Site;
+            operate various features available on the Site; send emails; analyze
+            data; provide search results and links and assist in fulfilling your
+            orders. From time to time, we may share your personally identifiable
+            information in connection with strategic relationships. Some of
+            these strategic relationships may include operating a co-branded
+            website or web pages with a strategic partner. You should be aware
+            that these co-branded sites may collect different or additional
+            information. Additionally, we may provide links on our Site to
+            websites controlled by third parties. We have no control over these
+            websites and our privacy policy does not apply to these third party
+            websites. Please consult the privacy policy of the website you are
+            visiting. Also, we may share personally identifiable or other
+            information with our parent, subsidiaries, divisions, and
+            affiliates. Occasionally, we provide our postal mailing list
+            (consisting of customer names and postal mailing addresses, email
+            addresses) to other companies whose products we believe may be of
+            interest to you. In order to determine those products that we
+            believe may be of interest to you, the information that you give us
+            and information about your order may be combined with other
+            personally identifiable information (such as demographic information
+            and past purchase history) available from our records and other
+            sources. We may transfer personally identifiable information as an
+            asset in connection with a proposed or actual merger or sale
+            (including any transfers made as part of an insolvency or bankruptcy
+            proceeding) involving all or part of our business or as part of a
+            corporate reorganization, stock sale or other change in control. We
+            reserve the right to disclose information in order to comply with a
+            subpoena, court order, administrative or governmental order, or any
+            other requirement of law, or when we, in our sole discretion,
+            believe it is necessary in order to protect our rights or the rights
+            of others, to prevent harm to persons or property, to fight fraud
+            and credit risk reduction. We use non-personally identifiable
+            information in the aggregate, so that we can improve the Site and
+            for business and administrative purposes. We may also use or share
+            with third parties for any purpose aggregated data that contains no
+            personally identifiable information. How We Protect Your Information
+            We are committed to protecting the information we receive from you.
+            We follow reasonable technical and management practices to help
+            protect the confidentiality, security and integrity of data stored
+            on our system. While no computer system is completely secure, we
+            believe the measures we have implemented reduce the likelihood of
+            security problems to a level appropriate to the type of data
+            involved. The Site encrypts your credit card number and other
+            personal information using secure socket layer (SSL) technology to
+            provide for the secure transmission of the information from your PC
+            to our servers. In addition, only those employees and third parties
+            who need access to your information in order to perform their duties
+            are allowed such access. Accessing And Updating Your Personal
+            Information And Preferences If you are a registered user, you may
+            access and update your registration information and your preferences
+            to receive email or other communications from us by sending an email
+            to tejpan.biz@gmail.com. While we make efforts to accommodate
+            requests to restrict our use of your information, we reserve the
+            right to delete all or any portion of customer information if we are
+            not able to reasonably accommodate a requested restriction.
+          </p>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+export default Privacy;
