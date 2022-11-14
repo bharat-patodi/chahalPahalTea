4a4ead4af530b5d83c83973f53ed4b4d9c3da0b7
Bharat Patodi <bharatpatodi@gmail.com>
2022-11-14 12:31:07 +0530

    Add Returns page content

--- pages/returns.tsx
+++ pages/returns.tsx
@@ -8,22 +8,25 @@ const Returns = () => {
       <main>
         <div>
           <h1>Returns</h1>
-          <p>
-            Cancellation of order All orders placed through chahalpahaltea.com
-            are processed with a very short turnaround time and hence
-            cancellation of orders are not possible. Return policy Returns will
-            be accepted only for quality concerns or damage of items in transit.
-            Concerns should be raised to tejpan.biz@gmail.com within 48 hours of
-            receipt with details of the order. In case of qualitative
-            inferiority of product, total value of the product as well as
-            courier charges borne by customer will be refunded back to customer
-            account. Products acknowledged as ‘damaged’ or ‘defective’ will be
-            replaced by us on receipt of the damaged product. Once we receive
-            the item at our facility we will verify its condition and initiate
-            the process of refund/replacement accordingly. REFUND POLICY Refund
-            for Orders once placed shall not be possible under normal
-            circumstances once shipping is carried out. Please contact us by
-            email to tejpan.biz@gmail.com or in the customer care number
+            <h2>Cancellation of order</h2>
+            <p>All orders placed through
+            chahalpahaltea.com are processed with a very short turnaround time
+            and hence cancellation of orders are not possible.</p>
+            <h2>Return policy</h2>
+            <p>Returns will be accepted only for quality concerns or damage of
+            items in transit. Concerns should be raised to tejpan.biz@gmail.com
+            within 48 hours of receipt with details of the order. In case of
+            qualitative inferiority of product, total value of the product as
+            well as courier charges borne by customer will be refunded back to
+            customer account. Products acknowledged as ‘damaged’ or ‘defective’
+            will be replaced by us on receipt of the damaged product. Once we
+            receive the item at our facility we will verify its condition and
+            initiate the process of refund/replacement accordingly.</p>
+            <h2>REFUND
+            POLICY</h2>
+            <p>Refund for Orders once placed shall not be possible under
+            normal circumstances once shipping is carried out. Please contact us
+            by email to tejpan.biz@gmail.com or in the customer care number
             available in the home page for any queries. Shop our products online
             at www.chahalpahaltea.com from anywhere in India and avail fabulous
             offers and discounts. Follow us on our Social Media pages: for
@@ -31,8 +34,7 @@ const Returns = () => {
             https://www.instagram.com/chahal_pahal_tea/
             https://www.facebook.com/chahalpahaltea/ Visit our Blogs
             https://www.chahalpahaltea.com/blog/ for useful and interesting
-            information about Tea.
-          </p>
+            information about Tea.</p>
         </div>
       </main>
       <Footer />
