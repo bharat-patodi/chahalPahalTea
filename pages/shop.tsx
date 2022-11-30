fe05bccd95f67efdd2a4e62b2efc0bd81c0258ae
Bharat Patodi <bharatpatodi@gmail.com>
2022-11-30 16:22:06 +0530

    Add last image

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -52,8 +52,8 @@ const Shop = () => {
           />
           <ProductView
             name="All Flavours in One"
-            src="masala-tea.png"
-            details="Assam tea leaves. Strong aroma and taste. No artificial flavours. All natural ingredients."
+            src="all-flavours.png"
+            details=""
             href="https://rzp.io/i/Nsl9maW18"
             price={508}
             weight="250gm x 4 = 1 kg"
@@ -71,16 +71,21 @@ const ProductView = (props: any) => {
     <>
       <div className={styles.productView}>
         <h3>{props.name}</h3>
-        <img
-          src={props.src}
-          className={styles.productImage}
-        />
+        <img src={props.src} className={styles.productImage} />
         <p>{props.details}</p>
-        <p><span className={styles.highlightText}>Price:</span> {props.price}</p>
-        <p><span className={styles.highlightText}>Shipping:</span> {props.shipping}</p>
-        <p><span className={styles.highlightText}>Total:</span> {props.shipping + props.price}</p>
+        <p>
+          <span className={styles.highlightText}>Price:</span> {props.price}
+        </p>
+        <p>
+          <span className={styles.highlightText}>Shipping:</span>{" "}
+          {props.shipping}
+        </p>
+        <p>
+          <span className={styles.highlightText}>Total:</span>{" "}
+          {props.shipping + props.price}
+        </p>
         <label htmlFor="">
-          Weight:
+          <span className={styles.highlightText}>Weight:</span>
           {/* <select name="quantity" id="">
             <option value="250gm">250gm</option>
             <option value="500gm">500gm</option>
@@ -98,7 +103,10 @@ const ProductView = (props: any) => {
           {/* <button>Add to Cart</button> */}
         </a>
         {/* </Link> */}
-        <p className={styles.disclaimer}>*After payment, please whatsapp/message your complete postal address on this number: +91 90990-90191</p>
+        <p className={styles.disclaimer}>
+          *After payment, please whatsapp/message your complete postal address
+          on this number: +91 90990-90191
+        </p>
       </div>
     </>
   );
