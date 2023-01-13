5a926735006a6290a91544737d8653b0b5d0fcb3
Bharat Patodi <bharatpatodi@gmail.com>
2023-01-13 14:20:29 +0530

    Add cart functionality

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -13,6 +13,41 @@ const Cart = () => {
     cart.adrakChai.gm1000.price * cart.adrakChai.gm1000.quantity +
     cart.adrakChai.gm500.price * cart.adrakChai.gm500.quantity +
     cart.adrakChai.gm250.price * cart.adrakChai.gm250.quantity;
+  let result = Object.values(cart);
+  const checkQuantity = (cart: {
+    [key: string]: {
+      name: string;
+      details: string;
+      src: string;
+      shipping: number;
+      gm250: { price: number; quantity: number };
+      gm500: { price: number; quantity: number };
+      gm1000: { price: number; quantity: number };
+    };
+  }): {
+    name: string;
+    details: string;
+    src: string;
+    shipping: number;
+    gm250: { price: number; quantity: number };
+    gm500: { price: number; quantity: number };
+    gm1000: { price: number; quantity: number };
+  }[] => {
+    return Object.values(cart).filter((chai) => {
+      return Object.values(chai)
+        .filter((val) => typeof val === "object")
+        .some((weight) => weight?.quantity > 0);
+    });
+  };
+  console.log(
+    "hi",
+    Object.values(cart).map((chai) => {
+      return Object.values(chai)
+        .filter((val) => typeof val === "object")
+        .some((weight) => weight.quantity > 0);
+    })
+  );
+  console.log(result);
   // console.log(
   //   cart.adrakChai.gm1000.price,
   //   cart.adrakChai.gm1000.quantity,
@@ -31,20 +66,81 @@ const Cart = () => {
       <main className="container">
         <div>
           <h1>Cart</h1>
-          <h2>Coming Soon</h2>
-          <div className={styles.cartDashboard}>
-            <section>
-              <p>Total: {total}</p>
-              {/* {Object.values(cart).map(val => {
+          <section className={styles.cartDashboard}>
+            <section className={styles.cartCard}>
+              <p>Product</p>
+              <p>Weight</p>
+              <p>Quantity</p>
+              <p>Price (INR)</p>
+            </section>
+            {/* <p>Total: {total}</p> */}
+            {console.log(
+              Object.values(cart).filter((chai) => {
+                return Object.values(chai)
+                  .filter((val) => typeof val === "object")
+                  .some((weight) => weight.quantity > 0);
+              })
+            )}
+            {Object.values(cart)
+              .filter((chai) => {
+                return Object.values(chai)
+                  .filter((val) => typeof val === "object")
+                  .some((weight) => weight.quantity > 0);
+              })
+              .map((currChai) => {
+                if (currChai.gm250.quantity > 0) {
+                  return (
+                    <section className={styles.cartCard}>
+                      <div className={styles.cartCardName}>
+                        <img src={currChai.src} width="100px" />
+                        <p>{currChai.name}</p>
+                      </div>
+                      <p>250 gm</p>
+                      <p>{currChai.gm250.quantity}</p>
+                      <p>
+                        INR {currChai.gm250.price * currChai.gm250.quantity}
+                      </p>
+                    </section>
+                  );
+                }
+                if (currChai.gm500.quantity > 0) {
+                  return (
+                    <section className={styles.cartCard}>
+                      <img src={currChai.src} width="100px" />
+                      <p>{currChai.name}</p>
+                      <p>500 gm</p>
+                      <p>{currChai.gm500.quantity}</p>
+                      <p>
+                        INR {currChai.gm500.price * currChai.gm500.quantity}
+                      </p>
+                    </section>
+                  );
+                }
+                if (currChai.gm1000.quantity > 0) {
+                  return (
+                    <section className={styles.cartCard}>
+                      <img src={currChai.src} width="100px" />
+                      <p>{currChai.name}</p>
+                      <p>1000 gm</p>
+                      <p>{currChai.gm1000.quantity}</p>
+                      <p>
+                        INR {currChai.gm1000.price * currChai.gm1000.quantity}
+                      </p>
+                    </section>
+                  );
+                }
+                return "";
+              })}
+            {/* {Object.values(cart).map(val => {
                 return <p key={1}>{val.name}</p>;
               })} */}
-              {/* {Object.values(cart).filter((val) => val).map(val => {
+            {/* {Object.values(cart).filter((val) => val).map(val => {
                 if(val.quantity > 0) {
                   return <p>Yes</p>
                 } else return <p>No</p>
                 })} */}
-              {/* mine.filter(val => val.name === "Premium Tea") */}
-              <p>Adrak 500 qtty: {cart.adrakChai.gm500.quantity}</p>
+            {/* mine.filter(val => val.name === "Premium Tea") */}
+            {/* <p>Adrak 500 qtty: {cart.adrakChai.gm500.quantity}</p>
               <p>Adrak 500 price: {cart.adrakChai.gm500.price}</p>
               <p>
                 Adrak 500 total:{" "}
@@ -56,9 +152,12 @@ const Cart = () => {
                 Adrak 1000 total:{" "}
                 {cart.adrakChai.gm1000.price * cart.adrakChai.gm1000.quantity}
               </p>
-              {console.log(cart.adrakChai.gm250.price)}
+              {console.log(cart.adrakChai.gm250.price)} */}
+            <section className={styles.cartCard}>
+              <p>Total</p>
+              <p>INR {}</p>
             </section>
-          </div>
+          </section>
         </div>
       </main>
       <Footer />
